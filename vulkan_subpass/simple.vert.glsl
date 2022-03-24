02ee91b0fe4862ec7463f82f5e9b9c04f701f555
Yuehang Wu <yuehan9.wu@gmail.com>
2022-03-24 01:22:55 +0000

    simple subpass example

---
+++ vulkan_subpass/simple.vert.glsl
@@ -0,0 +1,10 @@
+#version 460
+
+layout(location = 0) in vec4 vPosition;
+layout(location = 1) in vec2 vCoordinate;
+layout(location = 0) out vec2 fCoordinate;
+
+void main() {
+	gl_Position = vPosition;
+	fCoordinate = vCoordinate;
+}
