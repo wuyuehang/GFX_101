90df202664c8a6c0c04f2224541cf0b3b047fc14
Yuehang Wu <yuehan9.wu@gmail.com>
2022-03-26 13:53:54 +0000

    multiple descriptor set

---
+++ vulkan_multi_descriptor_set/multi_descriptor_set.frag.glsl
@@ -0,0 +1,12 @@
+#version 460
+
+layout(location = 0) in vec2 fCoordinate;
+layout(location = 0) out vec4 SV_Target;
+
+layout(set = 0, binding = 0) uniform texture2D tex;
+layout(set = 1, binding = 0) uniform sampler sam;
+
+void main()
+{
+	SV_Target = texture(sampler2D(tex, sam), fCoordinate);
+}
