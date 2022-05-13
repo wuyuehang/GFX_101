a46c08e1bfd5c6f45ea2d19ba7b4858fe077cb38
Yuehang Wu <yuehan9.wu@gmail.com>
2022-05-13 23:30:22 +0100

    [gl api]
    gl mrt

---
+++ api/gl_mrt/main.cpp
@@ -0,0 +1,123 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cassert>
+#include <cstring>
+#include <fstream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include "Controller.hpp"
+#include "Mesh.hpp"
+#include "Program.hpp"
+#include "Util.hpp"
+
+util::AssimpMesh mesh;
+
+GLuint VAO; // offscreen vertex array object
+GLuint FBO; // offscreen framebuffer
+GLuint TEX[2]; // offscreen mrt colorbuffer (texture)
+GLuint RB; // offscreen depthbuffer (renderbuffer)
+util::Program *P;
+
+void init_offscreen() {
+    glGenTextures(2, TEX);
+    // albeto (diffuse) texture
+    glBindTexture(GL_TEXTURE_2D, TEX[0]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 1024, 768, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // roughness texture
+    glBindTexture(GL_TEXTURE_2D, TEX[1]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 1024, 768, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // depth renderbuffer
+    glGenRenderbuffers(1, &RB);
+    glBindRenderbuffer(GL_RENDERBUFFER, RB);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, 1024, 768);
+
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, FBO);
+    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, TEX[0], 0); // albeto
+    assert(GL_FRAMEBUFFER_COMPLETE == glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER));
+    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, TEX[1], 0); // roughness
+    assert(GL_FRAMEBUFFER_COMPLETE == glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER));
+    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, RB);
+    assert(GL_FRAMEBUFFER_COMPLETE == glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER));
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, pos));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, nor));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, uv));
+    glEnableVertexAttribArray(2);
+
+    std::vector<std::string> shaders { "./mrt.vert", "./mrt.frag" };
+    P = new util::Program(shaders);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "gl mrt", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glewInit();
+
+    util::Controller *ctrl = new util::TrackballController(window);
+    mesh.load("../../assets/gltf/metal_cup_ww2_style_cup_vintage/scene.gltf");
+
+    init_offscreen();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ctrl->handle_input();
+
+        // PASS 1: render mrt offscreen
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, FBO);
+        GLenum cbuffers[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+        glDrawBuffers(2, cbuffers);
+        glViewport(0, 0, 1024, 768);
+        glDisable(GL_SCISSOR_TEST);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glClearColor(0.2, 0.2, 0.3, 1.0);
+        glClearDepthf(1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        P->setMat4("model_mat", ctrl->get_model() * mesh.get_model_mat());
+        P->setMat4("view_mat", ctrl->get_view());
+        P->setMat4("proj_mat", ctrl->get_proj());
+        P->use();
+        glBindVertexArray(VAO);
+        mesh.draw(P);
+
+        // PASS 2: onscreen
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        glViewport(0, 0, 1024, 768);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glClearColor(0.2, 0.2, 0.3, 1.0);
+        glClearDepthf(1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_SCISSOR_TEST);
+        // albeto
+        glScissor(0, 0, 1024/2, 768);
+        gltest::DrawTexture(TEX[0]);
+        // roughness
+        glScissor(1024/2, 0, 1024/2, 768);
+        gltest::DrawTexture(TEX[1]);
+        glfwSwapBuffers(window);
+    }
+
+    delete P;
+    delete ctrl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
