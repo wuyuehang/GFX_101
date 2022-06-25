59355237255dc5c26d43c45fbb93e00bf423d1e5
Yuehang Wu <yuehan9.wu@gmail.com>
2022-06-25 10:37:35 +0100

    [IR] GEP load store

--- ir/GEP.cpp
+++ ir/GEP.cpp
@@ -13,7 +13,7 @@ int main() {
         {
             SmallVector<Type*, 8> argTypes;
             Type *t_1xi32 = Type::getInt32Ty(TheContext);
-            Type *t_2xi32 = FixedVectorType::get(t_1xi32, 2); t_2xi32->dump();
+            Type *t_2xi32 = FixedVectorType::get(t_1xi32, 2);
             Type *t_ptr_1xi32 = PointerType::get(t_1xi32, 0);
             Type *t_ptr_2xi32 = t_2xi32->getPointerTo(0);
             argTypes.push_back(t_ptr_1xi32);
@@ -36,6 +36,20 @@ int main() {
             for (auto AI = func->arg_begin(), AE = func->arg_end(); AI != AE; AI++, i++) {
                 AI->setName(argNames[i]);
             }
+            verifyFunction(*func);
+        }
+        {
+            Type *t_1xi32 = Type::getInt32Ty(TheContext);
+            Type *t_ptr_1xi32 = PointerType::get(t_1xi32, 0);
+            FunctionType *funcType = FunctionType::get(TheBuilder.getInt32Ty(), t_ptr_1xi32, false);
+            Function *func = Function::Create(funcType, Function::ExternalLinkage, "test1", TheModule);
+
+            BasicBlock *entryBB = BasicBlock::Create(TheContext, "entry", func);
+            TheBuilder.SetInsertPoint(entryBB);
+            auto *loadAddr = TheBuilder.CreateGEP(TheBuilder.getInt32Ty(), func->getArg(0), TheBuilder.getInt32(0));
+            auto *loadValue = TheBuilder.CreateLoad(t_1xi32, loadAddr);
+            TheBuilder.CreateStore(loadValue, loadAddr);
+            TheBuilder.CreateRet(TheBuilder.getInt32(0));
             verifyFunction(*func);
         }
     }
