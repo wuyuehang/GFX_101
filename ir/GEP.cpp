4a80ef933bc55a3396be461fd1ec065583cca159
Yuehang Wu <yuehan9.wu@gmail.com>
2022-06-25 11:23:19 +0100

    [IR] GEP extract and insert element

--- ir/GEP.cpp
+++ ir/GEP.cpp
@@ -52,6 +52,30 @@ int main() {
             TheBuilder.CreateRet(TheBuilder.getInt32(0));
             verifyFunction(*func);
         }
+        {
+            Type *t_1xi32 = Type::getInt32Ty(TheContext);
+            Type *t_2xi32 = FixedVectorType::get(t_1xi32, 2);
+            Type *t_ptr_2xi32 = t_2xi32->getPointerTo(0);
+            FunctionType *funcType = FunctionType::get(TheBuilder.getVoidTy(), {t_ptr_2xi32, t_ptr_2xi32}, false);
+            Function *func = Function::Create(funcType, Function::ExternalLinkage, "test2", TheModule);
+
+            BasicBlock *entryBB = BasicBlock::Create(TheContext, "entry", func);
+            TheBuilder.SetInsertPoint(entryBB);
+            auto *loadAddr = TheBuilder.CreateGEP(TheBuilder.getInt32Ty(), func->getArg(0), TheBuilder.getInt32(0));
+            auto *loadValue = TheBuilder.CreateLoad(t_2xi32, loadAddr);
+            auto *V0 = TheBuilder.CreateExtractElement(loadValue, TheBuilder.getInt64(0));
+            auto *V1 = TheBuilder.CreateExtractElement(loadValue, TheBuilder.getInt64(1));
+
+            SmallVector<Constant *, 2> undef;
+            undef.push_back(TheBuilder.getInt32(12));
+            undef.push_back(TheBuilder.getInt32(0));
+            loadAddr = TheBuilder.CreateGEP(TheBuilder.getInt32Ty(), func->getArg(1), TheBuilder.getInt32(0),"st_addr");
+            auto *newValue = TheBuilder.CreateLoad(t_2xi32, loadAddr);
+            TheBuilder.CreateInsertElement(newValue, V1, TheBuilder.getInt64(0));
+            TheBuilder.CreateInsertElement(newValue, V0, TheBuilder.getInt64(1));
+            TheBuilder.CreateStore(newValue, loadAddr);
+            verifyFunction(*func);
+        }
     }
 
     TheModule->dump();
